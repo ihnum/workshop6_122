f7ec7f8303fe5d1b36a822306c1c0cc82faa1370
unknown <ihnumscacu@gmail.com>
2026-09-25 10:45:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: TabBarView(
+          children: [
+            Container(),
+            Container(),
+          ],
+          ),
+          backgroundColor:const Color.fromARGB(255, 49, 224, 157),
+          bottomNavigationBar: const TabBar(
+            tabs: [
+              Tab(text: "บันทึกคะแนน"),
+              Tab(text: "รายชื่อนักเรียน"),
+            ],
+            labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            labelColor: Color.fromARGB(255, 255, 255, 255),
+          ),
+    ),
+    );
+  }
+}
